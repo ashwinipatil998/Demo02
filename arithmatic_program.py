9667a9dca67b3260e5c9f4c1dea3500522996c6d
sujit yadav <sujityadav99@gmail.com>
2022-12-04 23:54:49 +0530

    added code for AMZ-OO1

--- arithmatic_program.py
+++ arithmatic_program.py
@@ -1,3 +1,7 @@
 def add(a,b):
     return a+b
-print('t    he addition of a & b is', add (7,8))
+print('the addition of a & b is', add (7,8))
+
+def substraction(a,b):
+    return a-b
+print(f'the substraction of a and b is', substraction(17,8))
